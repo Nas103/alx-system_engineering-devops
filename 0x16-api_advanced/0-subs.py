8bf277a4190dccf07a78a4cd531523971a09e1dc
nas103 <tumelorhulani33@gmail.com>
2024-08-13 22:32:54 +0000

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,24 @@
-i!/usr/bin/python3
-"""
-Queries subscribers on reddit subreddit.
-"""
+#!/usr/bin/python3
 import requests
 
-def number_of_subcribers(subreddit):
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Mozilla/5.0"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.staus_code == 200:
-		data = response.json()
-		subscribers = data['data']['subscribers']
-		return subscribers
-	else:
-		return 0
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            return subscribers
+        else:
+            print("Error: Received status code", response.status_code)
+            return 0
+    except requests.exceptions.RequestException as e:
+        print("Request failed:", e)
+        return 0
